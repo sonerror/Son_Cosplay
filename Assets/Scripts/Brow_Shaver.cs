ac5ecdc4ee4e10832ea4613321171e023ac5ab5b
Dnbk3 <phucdt504@gmail.com>
2025-11-25 18:34:24 +0700

    ...

--- Assets/Scripts/Brow_Shaver.cs
+++ Assets/Scripts/Brow_Shaver.cs
@@ -1,16 +1,83 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Brow_Shaver : Item
 {
-  // Start is called once before the first execution of Update after the MonoBehaviour is created
-  void Start()
-  {
+  public Collider2D triggerCollider;
+  public Vector2 Angle = new Vector2(0, 0);
+  public Renderer rende;
+  private int _oriOrder;
+  private Vector3 _prePos;
 
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  public override void MouseDown(BaseEventData eventData)
+  {
+    if (isBlocked) return;
+    if (isDragging) return;
+    if (!IsReady) { OnWrong?.Invoke(); }
+    else
+    {
+      triggerCollider.enabled = true;
+    }
+    base.MouseDown(eventData);
+
+    OnHandleMouseDown();
   }
 
-  // Update is called once per frame
-  void Update()
+  public override void MouseUp(BaseEventData eventData)
   {
+    if (isBlocked) return;
+    if (!isDragging) return;
+    base.MouseUp(eventData);
 
+    isDragging = false;
+    OnDropItem?.Invoke();
+
+    Tf.DOKill();
+    Tf.DOScale(Vector3.one, 0.2f);
+    Tf.DORotate(Vector3.forward * Angle.x, 0.2f);
+    Tf.DOMove(_prePos, 0.3f).SetEase(Ease.OutBack).OnComplete(() =>
+{
+  ChangeLayerDown();
+});
+
+
+    triggerCollider.enabled = false;
+  }
+
+  public override void MouseDrag(BaseEventData eventData)
+  {
+    if (isBlocked || !isDragging) return;
+    Vector3 targetPos = GetMouseWorldPos() + offSet;
+    Vector3 pos = Tf.position;
+    pos = Vector3.Lerp(pos, targetPos, 0.4f * Time.deltaTime * 50f);
+    Tf.position = pos;
+  }
+
+  void OnHandleMouseDown()
+  {
+    isDragging = true;
+    OnPickItem?.Invoke();
+    SoundManager.Ins.PlayFx(FxType.Click);
+
+    Vector3 mouseWorldPos = GetMouseWorldPos();
+    offSet = Tf.position - mouseWorldPos;
+    Vector3 targetPos = mouseWorldPos + offSet;
+    targetPos.z = 0;
+
+    Tf.DOKill();
+    Tf.DOScale(Vector3.one * 1.15f, 0.2f);
+    Tf.DORotate(Vector3.forward * Angle.y, 0.2f);
+    ChangeLayerUp();
+  }
+  void ChangeLayerUp()
+  {
+    rende.sortingOrder = _oriOrder + 100;
+  }
+
+  void ChangeLayerDown()
+  {
+    rende.sortingOrder = _oriOrder;
   }
 }
